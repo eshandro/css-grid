8d853e8362cdd7e4291c37c016080a4eda099ad0
eshandro <erik843@gmail.com>
2017-07-08 13:46:29 -0600

    Add scrolling abilty to modal and fix flex centering issue on overflow by flex item of flex container

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,19 +1,27 @@
 let modalOverlay = document.getElementById('modal-overlay'),
     closeModal = document.getElementById('close-modal'),
-    openModal = document.getElementById('open-modal');
+    openModal = document.getElementById('open-modal'),
+    restOfPage = document.getElementById('secondaryPageContainer') || document.getElementById('homepagePageContainer');
 
 closeModal.addEventListener('click', function () {
-  modalOverlay.classList.toggle('modal-hidden');
+	modalOverlay.classList.toggle('modal-hidden');
+	modalOverlay.setAttribute("aria-hidden", "true");
+	// Turn scrolling back on for rest of content
+	restOfPage.classList.toggle('noScroll');
 });
 openModal.addEventListener('click', function () {
-  modalOverlay.classList.toggle('modal-hidden');
+	modalOverlay.classList.toggle('modal-hidden');
+	modalOverlay.setAttribute("aria-hidden", "false");
+	// Prevent rest of content from scrolling
+	// console.log(this.clientTop());
+	restOfPage.classList.toggle('noScroll');
 });
-// Handle ESC key (key code 27) to close modal
+// Allow ESC key (key code 27) to close modal
 document.addEventListener('keyup', function (e) {
-  if (e.keyCode == 27) {
-    if (!modalOverlay.classList.contains('modal-hidden')) {
-      modalOverlay.classList.toggle('modal-hidden');
-    }
-  }
+	if (e.keyCode == 27) {
+		if (!modalOverlay.classList.contains('modal-hidden')) {
+			modalOverlay.classList.toggle('modal-hidden');
+		}
+	}
 });
 //# sourceMappingURL=modal.js.map
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,14 +1,23 @@
 let 	modalOverlay = document.getElementById('modal-overlay'),
-		closeModal = document.getElementById('close-modal'),
-		openModal = document.getElementById('open-modal');
+		closeModal = 	document.getElementById('close-modal'),
+		openModal = 	document.getElementById('open-modal'),
+		restOfPage = 	document.getElementById('secondaryPageContainer') || 
+							document.getElementById('homepagePageContainer');
 
 closeModal.addEventListener('click', function() {
 	modalOverlay.classList.toggle('modal-hidden');
+	modalOverlay.setAttribute("aria-hidden", "true");
+	// Turn scrolling back on for rest of content
+	restOfPage.classList.toggle('noScroll');
 });
 openModal.addEventListener('click', function() {
 	modalOverlay.classList.toggle('modal-hidden');
+	modalOverlay.setAttribute("aria-hidden", "false");
+	// Prevent rest of content from scrolling
+	// console.log(this.clientTop());
+	restOfPage.classList.toggle('noScroll');
 });
-// Handle ESC key (key code 27) to close modal
+// Allow ESC key (key code 27) to close modal
 document.addEventListener('keyup', function(e) {
    if (e.keyCode == 27) {
    	if(!modalOverlay.classList.contains('modal-hidden')) {
